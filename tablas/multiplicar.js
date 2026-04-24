a08055d18e47d358ba31fab86a96bf1adcad751f
Anthony19H <anthony.fabian.herrera@hotmail.com>
2026-04-23 20:43:32 -0500

    sentencia for tabla del 5

---
+++ tablas/multiplicar.js
@@ -0,0 +1,17 @@
+function generarTablas(){
+
+    let titulo =document.getElementById("titulotxt")
+    let contenedor = document.getElementById("txtTabla");
+    let contenido ="";
+    let numero =5;
+
+    for(let i=1;i<=10 ;i++){
+        contenido += `
+            <div class="item">
+                <span class="op">${numero} x ${i}</span> 
+                <span class="res">${numero * i}</span>
+            </div>`;
+    }
+    contenedor.innerHTML = contenido;
+    titulo.innerHTML = "SISTEMA: TABLA "+ numero;
+}
